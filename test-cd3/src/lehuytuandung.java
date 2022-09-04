db4992ff8b9eba539d2a2267198d208a7fea65d6
bigdxd <65650019+bigdxd@users.noreply.github.com>
2022-09-04 22:50:50 +0700

    Create lehuytuandung.java

---
+++ test-cd3/src/lehuytuandung.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class tuandung {
+    public static void main (String[] args){
+        Scanner nhap = new Scanner(System.in);
+        System.out.println("Nhap n: ");
+        int n = nhap.nextInt();
+        int s = 0;
+        for (int i = 1; i <= n; i++){
+            s +=i;
+        }
+        System.out.println("Tong 1->n: "+s);
+    }
+}
+Footer
+© 2022 GitHub, Inc.
